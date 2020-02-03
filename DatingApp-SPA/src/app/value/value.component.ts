45f4eff0b06747c2cc6c8b2e0f4cb0fb6182b742
ariannetan <aj.yannie@gmail.com>
2020-02-03 22:06:27 +0800

    datingapp ui

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -7,10 +7,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
+  values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues()
+  }
+
+  getValues()
+  {
+    this.http.get("http://localhost:5000/api/values").subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,8 @@ namespace DatingApp.API
                 x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +46,8 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
