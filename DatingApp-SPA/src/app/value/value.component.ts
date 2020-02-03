d776d68b9e5cbbf5f04f03c6b642102960ee9b8b
ariannetan <aj.yannie@gmail.com>
2020-02-03 20:59:17 +0800

    datingapp ui

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
